96bf7422db8fa26b280a73077ebd1e2815a5c9e4
hank.dohwankim <hank.dohwankim@gmail.com>
2020-04-10 01:24:15 -0500

    initial commit

---
+++ WinnipegForum.Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WinnipegForum.Data.Models;
+
+namespace WinnipegForum.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Forum> Forums { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<PostReply> PostReplies { get; set; }
+        public DbSet<ReplyReply> ReplyReplies { get; set; }
+
+    }
+}
---
+++ WinnipegForum.Data/Models/ReplyReply.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WinnipegForum.Data.Models
+{
+    public class ReplyReply : PostReply
+    {
+        public PostReply PostReply { get; set; }
+    }
+}

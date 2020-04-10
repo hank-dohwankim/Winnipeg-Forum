60f133dc416998675b76fb12b4f40568ea12e3e5
hank.dohwankim <hank.dohwankim@gmail.com>
2020-04-10 01:45:13 -0500

    service layer added

---
+++ WinnipegForum.Service/ForumService.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WinnipegForum.Data;
+using WinnipegForum.Data.Models;
+
+namespace WinnipegForum.Service
+{
+    public class ForumService : IForum
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public ForumService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int forumId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _dbContext.Forums.Include(forum => forum.Posts);
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateForumTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
